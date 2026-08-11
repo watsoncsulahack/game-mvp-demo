84f7a6b93488030e6ea7fcdc97d4de47d067162d
watsoncsulahack <allan.rojas01@student.csulb.edu>
2026-08-11 15:16:16 -0700

    Remove unused Faucet loader

--- src/campus-faucet-loader.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  'use strict';
-  if (document.querySelector('script[data-campus-faucet-v3]')) return;
-  const script = document.createElement('script');
-  script.src = 'src/campus-faucet-integration-v3.js';
-  script.dataset.campusFaucetV3 = '';
-  document.head.appendChild(script);
-})();
