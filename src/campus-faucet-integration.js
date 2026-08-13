ba5d5f07c0c64349889515ba20ac11269c1eac2d
watsoncsulahack <allan.rojas01@student.csulb.edu>
2026-08-12 17:06:09 -0700

    Refine console assistant and course demo

--- src/campus-faucet-integration.js
+++ src/campus-faucet-integration.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const version = '20260812-5';
+  const version = '20260812-6';
   const styles = [
     ['campus-currencies', 'styles/campus-currencies.css'],
     ['campus-faucet-drops', 'styles/campus-faucet-drops.css'],
